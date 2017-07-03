1f67bda00d2370a9e19361b708be6782ec701eba
helio <helio@Notebook>
2017-07-03 19:47:51 -0300

    Foi feito a parte de exclusão e alteração via Get video aula 5

--- FabricaWeb/src/main/java/br/com/fabricadeprogarmador/controller/UsuarioController.java
+++ FabricaWeb/src/main/java/br/com/fabricadeprogarmador/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.fabricadeprogarmador.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,17 +20,42 @@ public class UsuarioController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	
+		
+		String acao = req.getParameter("acao");
+		
+		if(acao.equals("exc")){
+		
+			String id = req.getParameter("id");
+			Usuario usu = new Usuario();
+			if(id != null)
+				usu.setIdUsuario(Integer.parseInt(id));
+			
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarioDAO.excluir(usu);
+			
+			resp.getWriter().println("Excluido com Sucesso");
+		}else if(acao.equals("lis")){
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			List<Usuario> lista = usuarioDAO.buscarTodos();
+			resp.getWriter().println(lista);
+		}
+			
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		String id = req.getParameter("id");
 		String nome = req.getParameter("nome");
 		String login = req.getParameter("login");
 		String senha = req.getParameter("senha");
 		
 		Usuario usu = new Usuario();
+		
+		if(id != null)
+			usu.setIdUsuario(Integer.parseInt(id));
+			
 		usu.setNome(nome);
 		usu.setLogin(login);
 		usu.setSenha(senha);
